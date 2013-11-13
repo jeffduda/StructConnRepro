05025f30ff1533193e64bc2126f1d13036c5c2b3
Jeffrey Duda <jeff.duda@gmail.com>
2013-11-13 16:40:03 -0500

    ADD: petiole cmake file

---
+++ software/PetioleToolkit.remote.cmake
@@ -0,0 +1,5 @@
+itk_fetch_module(PetioleToolkit
+      "Framework for network analysis in medical images."
+        GIT_REPOSITORY https://github.com/jeffduda/Petiole.git
+          GIT_TAG 9660712f8826bf52f672c93ee968fd71e35e9aeb
+          )
